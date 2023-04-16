db66f15622df8fb7fb237232a56bf8201d4b8113
hyunganom <dlguswo0504@naver.com>
2023-04-16 19:02:46 +0900

    first upload

---
+++ JenkinsTest/src/main/java/com/jenkins/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.jenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class TestController {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String test(Model model) {
+		return "jenkins test";
+	}
+}
